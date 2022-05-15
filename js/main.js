21f6b9aa9642722deef5e88acc20ed222a459f72
MauricioMSoares <mauricio.soares@sptech.school>
2022-05-14 22:08:43 -0300

    Continuação na página home, header arrumado e banner adicionado.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Referência: https://www.youtube.com/watch?v=zUd_lUxe2k8
+
+/* var imagens = ["img01.png", "img02.png", "img03.png", "img04.png"];
+var imagematual = 0;
+
+function trocaimagem() {
+ imagematual = (imagematual + 1) % 4;
+document.querySelector('.gif img').src = imagens[imagematual];
+}
+setInterval(trocaimagem, 1500); */
