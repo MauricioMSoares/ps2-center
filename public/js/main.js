674c9b6f9d5807221998e53e43119991f0ac56a3
MauricioMSoares <mauricio.soares@sptech.school>
2022-05-20 14:40:13 -0300

    Reorganização dos repositórios para facilitar a adição da API

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+//Referência: https://www.youtube.com/watch?v=zUd_lUxe2k8
+
+var imagens = ["assets/banner-1.jpg", "assets/banner-2.jpg", 
+"assets/banner-3.jpg", "assets/banner-4.jpg", "assets/banner-5.jpg"];
+var imagemAtual = 0;
+var qtd_imagens = imagens.length;
+
+setInterval(trocar_imagem, 3000);
+
+function trocar_imagem() {
+    switch (imagemAtual) {
+        default:
+            caixa_banner.innerHTML = `<div id="banner" style="background-image: url('${imagens[0]}')">`;
+            break;
+        case 1:
+            caixa_banner.innerHTML = `<div id="banner" style="background-image: url('${imagens[1]}')">`;
+            break;
+        case 2:
+            caixa_banner.innerHTML = `<div id="banner" style="background-image: url('${imagens[2]}')">`;
+            break;
+        case 3:
+            caixa_banner.innerHTML = `<div id="banner" style="background-image: url('${imagens[3]}')">`;
+            break;
+        case 4:
+            caixa_banner.innerHTML = `<div id="banner" style="background-image: url('${imagens[4]}')">`;
+            break;
+    }
+    imagemAtual++;
+
+    if (imagemAtual == qtd_imagens) {
+        imagemAtual = 0;
+    }
+}
